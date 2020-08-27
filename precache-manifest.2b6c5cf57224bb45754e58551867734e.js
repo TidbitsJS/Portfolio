61c801ca8368ab336985c01fbfefbf5de1e1897e
sujata <gunalesujata@gmail.com>
2020-08-27 19:57:52 +0530

    Updates

---
+++ precache-manifest.2b6c5cf57224bb45754e58551867734e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74cae18421c3ed0aa71b4cf9bea97b37",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "f5a36513e03ee6ee8d53",
+    "url": "/Portfolio/static/css/main.1a163c34.chunk.css"
+  },
+  {
+    "revision": "8b45faab83004ddea68d",
+    "url": "/Portfolio/static/js/2.f91e0fab.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Portfolio/static/js/2.f91e0fab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5a36513e03ee6ee8d53",
+    "url": "/Portfolio/static/js/main.4fd56087.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "2ec409f52d691e3f0370ca6e33380886",
+    "url": "/Portfolio/static/media/me.2ec409f5.jpeg"
+  },
+  {
+    "revision": "af2f836e70a452b499447b1efa269693",
+    "url": "/Portfolio/static/media/project.af2f836e.jpeg"
+  }
+]);
